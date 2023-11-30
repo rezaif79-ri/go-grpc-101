abc23823b9b0f446d8706ada0dbbc362e554c913
Reza IF <rezaif79.ri@gmail.com>
2023-11-30 19:00:51 +0700

    feat: move and reimplement userService in service package

---
+++ server/service/user_service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rezaif79-ri/go-grpc-101/model/user"
+)
+
+// userService is a struct implementing UserServiceServer
+type userService struct {
+	user.UnimplementedUserServiceServer
+}
+
+func NewUserService() user.UserServiceServer {
+	return &userService{
+		UnimplementedUserServiceServer: user.UnimplementedUserServiceServer{},
+	}
+}
+
+// GreetUser return greeting message given the name and salutation
+// in gRPC protocol
+func (us *userService) GreetUser(ctx context.Context, req *user.GreetingRequest) (*user.GreetingResponse, error) {
+	//business logic
+	salutationMessage := fmt.Sprintf("Howdy, %s %s, nice to see you in the future!",
+		req.Salut, req.Name)
+
+	return &user.GreetingResponse{GreetingMessage: salutationMessage}, nil
+}
