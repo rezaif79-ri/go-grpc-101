58f101dc9f789336634f0a22b0b363d283f6bf0d
Reza IF <rezaif79.ri@gmail.com>
2023-12-01 17:56:21 +0700

    feat: add router package for setup router
    Add router package and add setup router function to orchestrate controller, service and model to fiber router

---
+++ aggregator/router/router.go
@@ -0,0 +1,31 @@
+package router
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/rezaif79-ri/go-grpc-101/aggregator/config"
+	"github.com/rezaif79-ri/go-grpc-101/aggregator/controller"
+	"github.com/rezaif79-ri/go-grpc-101/aggregator/service"
+	"github.com/rezaif79-ri/go-grpc-101/model/user"
+)
+
+func SetupRouter(app *fiber.App) {
+	// Init tcp conn
+	conn, err := config.NewServiceGrpcConn()
+	if err != nil {
+		log.Fatalf("Could not connect to service: %v", err)
+		return
+	}
+	// Init model
+	var userModel = user.NewUserServiceClient(conn)
+
+	// Init service
+	var userService = service.NewUserService(userModel)
+
+	// Init controller
+	var userController = controller.NewUserController(&userService)
+	// Set route and group
+	var user = app.Group("/api/users")
+	user.Post("greetings", userController.GreetUser)
+}
