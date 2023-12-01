f2399b508238452b44df63dce6571c11becab8eb
Reza IF <rezaif79.ri@gmail.com>
2023-12-01 17:53:03 +0700

    feat: add service config
    Add service config for initialize grpc dial connection

---
+++ aggregator/config/serviceConfig.go
@@ -0,0 +1,15 @@
+package config
+
+import (
+	"google.golang.org/grpc"
+)
+
+func NewServiceGrpcConn() (*grpc.ClientConn, error) {
+	//Create connection to gRPC server
+	conn, err := grpc.Dial("localhost:5001", grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+	return conn, err
+}
