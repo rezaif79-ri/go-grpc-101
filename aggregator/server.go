de41eee7a3bacb9aa9f2d57ef6959f4223b2d259
Reza IF <rezaif79.ri@gmail.com>
2023-12-01 18:04:41 +0700

    feat: add fiber config and logger, init setup router for fiber

--- aggregator/server.go
+++ aggregator/server.go
@@ -3,20 +3,23 @@ package main
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/gofiber/fiber/v2/middleware/recover"
+	"github.com/rezaif79-ri/go-grpc-101/aggregator/router"
 )
 
 const userServiceAddress = "localhost:5001"
 
 func main() {
 
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		EnablePrintRoutes: true,
+	})
 	app.Use(recover.New())
 	app.Use(cors.New())
+	app.Use(logger.New())
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+	router.SetupRouter(app)
 
-	app.Listen(":3000")
+	app.Listen("127.0.0.1:3210")
 }
