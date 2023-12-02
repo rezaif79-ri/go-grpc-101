a0ded5dba6c0b34429bc02766a2d4419e2f507bd
Reza IF <rezaif79.ri@gmail.com>
2023-12-02 23:09:22 +0700

    feat: add calculator route with controller and service implementation

--- aggregator/router/router.go
+++ aggregator/router/router.go
@@ -7,10 +7,15 @@ import (
 	"github.com/rezaif79-ri/go-grpc-101/aggregator/config"
 	"github.com/rezaif79-ri/go-grpc-101/aggregator/controller"
 	"github.com/rezaif79-ri/go-grpc-101/aggregator/service"
+	"github.com/rezaif79-ri/go-grpc-101/aggregator/util"
+	"github.com/rezaif79-ri/go-grpc-101/model/calculator"
 	"github.com/rezaif79-ri/go-grpc-101/model/user"
 )
 
 func SetupRouter(app *fiber.App) {
+	// Init utilities
+	validator := util.NewGoValidator()
+
 	// Init tcp conn
 	conn, err := config.NewServiceGrpcConn()
 	if err != nil {
@@ -20,13 +25,20 @@ func SetupRouter(app *fiber.App) {
 	}
 	// Init model
 	var userModel = user.NewUserServiceClient(conn)
+	var calcModel = calculator.NewCalculatorServiceClient(conn)
 
 	// Init service
 	var userService = service.NewUserService(userModel)
+	var calcService = service.NewCalculatorService(calcModel)
 
 	// Init controller
 	var userController = controller.NewUserController(&userService)
+	var calcController = controller.NewCalculatorController(*validator, &calcService)
+
 	// Set route and group
-	var user = app.Group("/api/users")
-	user.Post("greetings", userController.GreetUser)
+	var userRoute = app.Group("/api/users")
+	userRoute.Post("greetings", userController.GreetUser)
+
+	var calculatorRoute = app.Group("/api/calculators")
+	calculatorRoute.Post("", calcController.Calculate)
 }
